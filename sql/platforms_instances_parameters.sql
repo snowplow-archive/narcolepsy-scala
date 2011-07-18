b3de9d24b0bff432a626d3ef9baa21ff5394fe0c
Alexander Dean <alexander.dean@keplarllp.com>
2011-07-18 16:13:24 +0100

    Updated to include the new poll_frequency field, and ID sequencing

--- sql/platforms_instances_parameters.sql
+++ sql/platforms_instances_parameters.sql
@@ -1,37 +1,35 @@
-/*
-Navicat PGSQL Data Transfer
+DROP TABLE "public"."platforms";
+CREATE TABLE "public"."platforms" (
+"id" int NOT NULL UNIQUE,
+"platform_name" varchar(256) NOT NULL,
+"platform_connector" varchar(256) NOT NULL
+)
+WITH (OIDS=FALSE);
 
-Source Server         : localhost PostgreSQL
-Source Server Version : 80408
-Source Host           : localhost:5432
-Source Database       : ord_erp_dev
-Source Schema         : public
+INSERT INTO "public"."platforms" VALUES ('1', 'PrestaShop', 'PrestaShopConnector');
+INSERT INTO "public"."platforms" VALUES ('2', 'Amazon Marketplace', 'AmazonMarketplaceConnector');
 
-Target Server Type    : PGSQL
-Target Server Version : 80408
-File Encoding         : 65001
+DROP TABLE "public"."platform_instances";
+CREATE TABLE "public"."platform_instances" (
+"id" int NOT NULL UNIQUE,
+"platform_instance_name" varchar(256) NOT NULL,
+"platform_id" integer NOT NULL REFERENCES "public"."platforms"("id"),
+"poll_frequency" integer NULL
+)
+WITH (OIDS=FALSE);
 
-Date: 2011-07-16 21:43:15
-*/
+INSERT INTO "public"."platform_instances" VALUES ('1', 'Psychic Bazaar PrestaShop', '1', '15');
+INSERT INTO "public"."platform_instances" VALUES ('2', 'Psychic Bazaar Amazon Marketplace UK', '60');
 
-
--- ----------------------------
--- Table structure for "public"."platform_instance_parameters"
--- ----------------------------
 DROP TABLE "public"."platform_instance_parameters";
 CREATE TABLE "public"."platform_instance_parameters" (
-"id" int8 NOT NULL,
-"platform_instance_id" int8 NOT NULL,
+"id" int NOT NULL UNIQUE,
+"platform_instance_id" integer NOT NULL REFERENCES "public"."platform_instances"("id"),
 "parameter_name" varchar(128) NOT NULL,
 "parameter_value" varchar(128)
 )
-WITH (OIDS=FALSE)
+WITH (OIDS=FALSE);
 
-;
-
--- ----------------------------
--- Records of platform_instance_parameters
--- ----------------------------
 INSERT INTO "public"."platform_instance_parameters" VALUES ('1', '1', 'api_key', 'R1R6NC8GB3CPXDPNFZ87N7T8LQ30SRH1');
 INSERT INTO "public"."platform_instance_parameters" VALUES ('2', '1', 'api_url', 'http://test.psychicbazaar.com/api');
 INSERT INTO "public"."platform_instance_parameters" VALUES ('3', '2', 'access_key_id', 'AKIAIDNIJIVSRTI5ATLQ');
@@ -40,59 +38,26 @@ INSERT INTO "public"."platform_instance_parameters" VALUES ('5', '2', 'merchant_
 INSERT INTO "public"."platform_instance_parameters" VALUES ('6', '2', 'marketplace_id', 'A1F83G8C2ARO7P');
 INSERT INTO "public"."platform_instance_parameters" VALUES ('7', '2', 'marketplace_locale', 'GB');
 
--- ----------------------------
--- Table structure for "public"."platform_instances"
--- ----------------------------
-DROP TABLE "public"."platform_instances";
-CREATE TABLE "public"."platform_instances" (
-"id" int8 NOT NULL,
-"platform_instance_name" varchar(256) NOT NULL,
-"platform_id" int8 NOT NULL
-)
-WITH (OIDS=FALSE)
+CREATE SEQUENCE "public"."platforms_id_seq";
 
-;
+CREATE SEQUENCE "public"."platform_instances_id_seq";
 
--- ----------------------------
--- Records of platform_instances
--- ----------------------------
-INSERT INTO "public"."platform_instances" VALUES ('1', 'Psychic Bazaar PrestaShop', '1');
-INSERT INTO "public"."platform_instances" VALUES ('2', 'Psychic Bazaar Amazon Marketplace UK', '2');
+CREATE SEQUENCE "public"."platform_instance_parameters_id_seq";
 
--- ----------------------------
--- Table structure for "public"."platforms"
--- ----------------------------
-DROP TABLE "public"."platforms";
-CREATE TABLE "public"."platforms" (
-"id" int8 NOT NULL,
-"platform_name" varchar(256) NOT NULL,
-"platform_connector" varchar(256) NOT NULL
-)
-WITH (OIDS=FALSE)
+ALTER TABLE "public"."platforms" ADD PRIMARY KEY ("id");
 
-;
+ALTER TABLE "public"."platforms"
+    ALTER COLUMN "id"
+        SET DEFAULT NEXTVAL('platforms_id_seq');
 
--- ----------------------------
--- Records of platforms
--- ----------------------------
-INSERT INTO "public"."platforms" VALUES ('1', 'PrestaShop', 'PrestaShopConnector');
-INSERT INTO "public"."platforms" VALUES ('2', 'Amazon Marketplace', 'AmazonMarketplaceConnector');
-
--- ----------------------------
--- Alter Sequences Owned By 
--- ----------------------------
-
--- ----------------------------
--- Primary Key structure for table "public"."platform_instance_parameters"
--- ----------------------------
-ALTER TABLE "public"."platform_instance_parameters" ADD PRIMARY KEY ("id");
-
--- ----------------------------
--- Primary Key structure for table "public"."platform_instances"
--- ----------------------------
 ALTER TABLE "public"."platform_instances" ADD PRIMARY KEY ("id");
 
--- ----------------------------
--- Primary Key structure for table "public"."platforms"
--- ----------------------------
-ALTER TABLE "public"."platforms" ADD PRIMARY KEY ("id");
+ALTER TABLE "public"."platform_instances"
+    ALTER COLUMN "id"
+        SET DEFAULT NEXTVAL('platform_instances_id_seq');
+
+ALTER TABLE "public"."platform_instance_parameters" ADD PRIMARY KEY ("id");
+
+ALTER TABLE "public"."platform_instance_parameters"
+    ALTER COLUMN "id"
+        SET DEFAULT NEXTVAL('platform_instance_parameters_id_seq');
