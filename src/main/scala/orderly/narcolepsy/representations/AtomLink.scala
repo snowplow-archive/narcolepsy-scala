59012b76b7103c3027085d09b24993550359b588
Alexander Dean <alexander.dean@keplarllp.com>
2011-11-27 10:55:31 +0200

    Added new AtomLink for HATEOASness

---
+++ src/main/scala/orderly/narcolepsy/representations/AtomLink.scala
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2011 Orderly Ltd. All rights reserved.
+ *
+ * This program is licensed to you under the Apache License Version 2.0,
+ * and you may not use this file except in compliance with the Apache License Version 2.0.
+ * You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the Apache License Version 2.0 is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
+ */
+package orderly.narcolepsy.representations
+
+// Scala
+import scala.reflect.BeanProperty
+
+// JAXB and XML
+import javax.xml.bind.annotation.XmlAttribute
+import javax.xml.bind.annotation.XmlRootElement
+
+/**
+ * Singleton to construct an AtomLink. (Content) type defaults
+ * to application/xml if not explicitly set
+ */
+object AtomLink {
+
+  /**
+   * Convenience constructor for an AtomLink
+   */
+  def apply(
+    rel:  String,
+    href: String,
+    `type`: String = "application/xml"): AtomLink = {
+
+    val al = new AtomLink
+    al.rel = rel
+    al.href = href
+    al.`type` = `type`
+
+    al // Return the new AtomLink bean
+  }
+}
+
+/**
+ * AtomLink is used to define a HATEOAS-friendly URL to access the detail
+ * on a specific resource. For more on this see this excellent blog post:
+ * http://www.zienit.nl/blog/2010/01/rest/hateoas-by-example
+ */
+@XmlRootElement(name="link", namespace="http://www.w3.org/2005/Atom")
+class AtomLink {
+
+  @XmlAttribute
+  @BeanProperty
+  var rel: String = _
+
+  @XmlAttribute
+  @BeanProperty
+  var href: String = _
+
+  @XmlAttribute
+  @BeanProperty
+  var `type`: String = _
+}
